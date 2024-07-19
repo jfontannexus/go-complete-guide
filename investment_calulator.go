83f10dfd4530399ad88d9ad62edb817b86a36efb
NEXUS\jfontan <jfontan@nexusgeographics.com>
2024-07-19 21:32:26 +0200

    add inflation as const

--- investment_calulator.go
+++ investment_calulator.go
@@ -1,23 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"math"
 )
 
 func main() {
 	// Call the function
-
+	const inflationRate = 2.0
 	var investedAmount, years float64 = 1000, 10 //multiple explicit type
 	expectedReturnRate := 5.5                    //infered type
 	var label, offset = "Investment", 10         //multiple inferred type
 	var result = CalculateInvestment(investedAmount, expectedReturnRate, years)
-	fmt.Println(offset, " The future value of the ", label, " is: ", result)
-
+	var inflationAdjustedResult = CalculateInvestment(investedAmount, expectedReturnRate-inflationRate, years)
+	printResults(offset, label, result, inflationAdjustedResult)
 }
 func CalculateInvestment(investedAmount float64, expectedReturnRate float64, years float64) float64 {
 	// Calculate the investment
-	return investedAmount *
-		math.Pow(1+expectedReturnRate/100, years)
+	return investedAmount * math.Pow(1+expectedReturnRate/100, years)
 
+}
+func printInvestmentResult(offset int, label string, result float64) {
+	// Print the result
+	println(label, ":", result)
+}
+func printResults(offset int, label string, result float64, inflationAdjustedResult float64) {
+	// Print the results
+	printInvestmentResult(offset, label, result)
+	printInvestmentResult(offset, label+" (Inflation Adjusted)", inflationAdjustedResult)
 }
