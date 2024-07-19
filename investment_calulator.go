fd4550294f986a8dac0b9083966792773057510f
NEXUS\jfontan <jfontan@nexusgeographics.com>
2024-07-19 21:55:16 +0200

    add scanner

--- investment_calulator.go
+++ investment_calulator.go
@@ -1,30 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"math"
 )
 
 func main() {
 	// Call the function
-	const inflationRate = 2.0
-	var investedAmount, years float64 = 1000, 10 //multiple explicit type
-	expectedReturnRate := 5.5                    //infered type
-	var label, offset = "Investment", 10         //multiple inferred type
+	const inflationRate, label = 2.0, "Investment Calculator"
+	var investedAmount, years float64
+	expectedReturnRate := 5.5 //infered type
+	scanAmount(investedAmount, "Enter the amount to invest: ")
+	scanAmount(years, "Enter the number of years to invest: ")
 	var result = CalculateInvestment(investedAmount, expectedReturnRate, years)
 	var inflationAdjustedResult = CalculateInvestment(investedAmount, expectedReturnRate-inflationRate, years)
-	printResults(offset, label, result, inflationAdjustedResult)
+	printResults(label, result, inflationAdjustedResult)
+}
+
+func scanAmount(amount float64, prompt string) {
+	// Read the amount from user input
+	print(prompt)
+	fmt.Scanln(&amount)
 }
 func CalculateInvestment(investedAmount float64, expectedReturnRate float64, years float64) float64 {
 	// Calculate the investment
 	return investedAmount * math.Pow(1+expectedReturnRate/100, years)
 
 }
-func printInvestmentResult(offset int, label string, result float64) {
+func printInvestmentResult(label string, result float64) {
 	// Print the result
 	println(label, ":", result)
 }
-func printResults(offset int, label string, result float64, inflationAdjustedResult float64) {
+func printResults(label string, result float64, inflationAdjustedResult float64) {
 	// Print the results
-	printInvestmentResult(offset, label, result)
-	printInvestmentResult(offset, label+" (Inflation Adjusted)", inflationAdjustedResult)
+	printInvestmentResult(label, result)
+	printInvestmentResult(label+" (Inflation Adjusted)", inflationAdjustedResult)
 }
